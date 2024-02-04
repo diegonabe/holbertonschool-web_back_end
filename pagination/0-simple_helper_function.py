8bacd01128a9b86b710558a25b0b1c7dfd606270
diegonabe <diegonabe@hotmail.com>
2024-02-04 13:57:18 -0500

    FIX PEP8 style

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -5,7 +5,7 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """ Devuelve el inicio y el final de index 
+    """ Devuelve el inicio y el final de index
      correspondiendo al rango de de index's """
     start = (page - 1) * page_size
     end = start + page_size
