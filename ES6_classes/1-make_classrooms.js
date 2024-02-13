432e8743a82e6afd5e0c00eb86b15a91e9117927
diego napan <130240874+diegonabe@users.noreply.github.com>
2024-02-13 15:52:45 -0500

    FIX ESLint

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,5 +1,5 @@
 // Importar la clase ClassRoom desde 0-classroom.js
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 function initializeRooms() {
   const room1 = new ClassRoom(19);
